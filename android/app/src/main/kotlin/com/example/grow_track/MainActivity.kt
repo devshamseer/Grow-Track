7814844f2cd6495c3ddafb7f61ac092b29e95344
devshamseer <111424565+devshamseer@users.noreply.github.com>
2025-04-07 16:15:15 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/grow_track/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.grow_track
+
+
+  import io.flutter.embedding.android.FlutterFragmentActivity
+
+            class MainActivity: FlutterFragmentActivity() {
+            }
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,24 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import flutter_local_notifications
+import flutter_timezone
+import local_auth_darwin
+import objectbox_flutter_libs
+import path_provider_foundation
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
+  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
+  ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
